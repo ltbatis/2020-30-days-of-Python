44512c4e242d87b58c12d6bee9ee6f667a7612c5
ltbatista <ltbatista30@gmail.com>
2020-01-03 13:06:46 -0300

    Day 2 Lists

---
+++ Days-2-3_Lists_Dictionaries_Tuples_Loops/lists.py
@@ -0,0 +1,21 @@
+abc = "some string"
+print(abc+abc)
+
+list = ["some list",123,"another string"]
+print(list)
+
+list.append("one more")
+print (list)
+
+list.pop()
+print(list)
+
+print("Lenght of list: {}".format(len(list)))
+print("Length of LUCAS: {}".format(len("LUCAS")))
+
+print("First (0 position) item of list: {}".format(list[0]))
+print("Second (1st position) item in list: {}".format(list[1]))
+
+abc = [1,2,3]
+print("ABC List: {}".format(abc))
+print("Last item from ABC: {}".format(abc[-1]))
